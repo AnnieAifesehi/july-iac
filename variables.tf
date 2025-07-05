c9816bde89c63b39aa8a25789d5ab1983e355fac
Annie <anita.aifesehi1@gmail.com>
2025-07-05 14:24:31 -0400

    Added VPC resources

--- variables.tf
+++ variables.tf
@@ -16,3 +16,37 @@ variable "environment" {
   type        = string
 }
 
+# VPC variables
+variable "vpc_cidr" {
+  description = "VPC cidr block"
+  type        = string
+}
+variable "public_subnet_az1_cidr" {
+  description = "Public subnet az1 cidr block"
+  type        = string
+}
+
+variable "public_subnet_az2_cidr" {
+  description = "Public subnet az2 cidr block"
+  type        = string
+}
+
+variable "private_app_subnet_az1_cidr" {
+  description = "Private app subnet az1 cidr block"
+  type        = string
+}
+
+variable "private_app_subnet_az2_cidr" {
+  description = "Private app subnet az2 cidr block"
+  type        = string
+}
+
+variable "private_data_subnet_az1_cidr" {
+  description = "Private data subnet az1 cidr block"
+  type        = string
+}
+
+variable "private_data_subnet_az2_cidr" {
+  description = "Private data subnet az2 cidr block"
+  type        = string
+}
---
+++ vpc.tf
@@ -0,0 +1,108 @@
+# create vpc
+# terraform aws create vpc
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name =  "${var.project_name}-${var.environment}-vpc" 
+  }
+}
+
+# create internet gateway and attach it to the vpc
+# terraform aws create internet gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-igw" 
+  }
+}
+
+# data source to get all availability zones in the region
+# terraform aws get availability zones
+data "aws_availability_zones" "available" {
+}
+
+# create public subnet az1
+# terraform aws create public subnet
+resource "aws_subnet" "public_subnet_az1" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = var.public_subnet_az1_cidr
+    availability_zone = data.aws_availability_zones.available.names[0]
+    map_public_ip_on_launch = true
+
+    tags = {
+      Name = "${var.project_name}-${var.environment}-public-subnet-az1"
+    }       
+}
+
+# create public subnet az2
+# terraform aws create public subnet
+resource "aws_subnet" "public_subnet_az2" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = var.public_subnet_az2_cidr
+    availability_zone = data.aws_availability_zones.available.names[1]
+    map_public_ip_on_launch = true
+
+    tags = {
+      Name = "${var.project_name}-${var.environment}-public-subnet-az2"
+
+    }       
+}
+
+# create route table and add public route
+# terraform aws create route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+    }
+
+    tags = {
+      Name = "${var.project_name}-${var.environment}-public-route-table"
+    }
+}
+
+# associate public subnet az1 to "public route table"
+# terraform aws associate route table with subnet
+resource "aws_route_table_association" "public_subnet_az1_association" {
+  subnet_id      = aws_subnet.public_subnet_az1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# associate public subnet az2 to "public route table"
+# terraform aws associate route table with subnet
+resource "aws_route_table_association" "public_subnet_az2_association" {
+  subnet_id      = aws_subnet.public_subnet_az2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# create private app subnet az1
+# terraform aws create subnet
+ resource  "aws_subnet" "private_app_subnet_az1" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = var.private_app_subnet_az1_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+    map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-private-app-subnet-az1"
+  }
+}
+
+# create private app subnet az2
+# terraform aws create subnet   
+resource "aws_subnet" "private_app_subnet_az2" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = var.private_app_subnet_az2_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+    map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-private-app-subnet-az2"
+  }
+}
